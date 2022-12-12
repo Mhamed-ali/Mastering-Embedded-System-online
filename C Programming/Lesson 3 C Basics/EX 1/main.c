aad776512a4e2f18c7fbf83ef53190142f2fc93e
Mohamed <mohamedklay918@gmail.com>
2022-12-12 22:59:19 +0200

    EX 1 and EX 2

---
+++ C Programming/Lesson 3 C Basics/EX 1/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 12, 2022
+ *      Author: Mohamed
+ */
+
+
+#include "stdio.h"
+
+int main(){
+	printf("C programming \n") ;
+	printf("###########################");
+}
---
+++ C Programming/Lesson 3 C Basics/EX 2/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 12, 2022
+ *      Author: Mohamed
+ */
+
+
+void main()
+{
+	int user_input;
+	printf("Enter a integer:");
+	scanf("%d",&user_input);
+	printf("You entered: %d",user_input) ;
+	printf("\n###########################");
+}
